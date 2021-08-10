1db10dadf967a65a4ea1d537c1ba1419535fd622
jerry <jerryhanjj@126.com>
2021-08-11 01:56:06 +0800

    更新 mytime3

---
+++ CPP/ch11/mytime3.cpp
@@ -0,0 +1,76 @@
+// mytime3.cpp -- implementing Time methods
+#include "mytime3.h"
+#include <iostream>
+
+Time::Time(/* args */) { hours = minutes = 0; }
+
+Time::Time(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+void Time::AddHr(int h)
+{
+    hours += h;
+}
+
+void Time::AddMin(int m)
+{
+    minutes += m;
+}
+
+void Time::Reset(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+Time Time::Sum(const Time &t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.minutes %= 60;
+    return sum;
+}
+
+void Time::Show() const
+{
+    std::cout << hours << " hours, " << minutes << "minutes";
+}
+
+Time Time::operator+(const Time &t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.minutes %= 60;
+    return sum;
+}
+
+Time Time::operator-(const Time &t) const
+{
+    Time diff;
+    int tot1, tot2;
+    tot1 = t.minutes + 60 * t.hours;
+    tot2 = minutes + 60 * hours;
+    diff.hours = (tot2 - tot1) / 60;
+    diff.minutes = (tot2 - tot1) % 60;
+    return diff;
+}
+
+Time Time::operator*(double n) const
+{
+    Time result;
+    long totalmin = hours * n * 60 + minutes * n;
+    result.minutes = totalmin % 60;
+    result.hours = totalmin / 60;
+    return result;
+}
+
+std::ostream & operator<<(std::ostream & os, const Time & t)
+{
+    os << t.hours << " hours, " << t.minutes << " minutes";
+    return os;
+}
