dcd50396b41ad6f25acd0c9a2fa02e6fc7453d1d
jerry <jerryhanjj@126.com>
2021-08-19 22:40:19 +0800

    增加 TableTennisPlayer及其派生类

---
+++ CPP/ch12/tabtenn0.cpp
@@ -0,0 +1,13 @@
+// tabtenn0.cpp -- simple base-class methods
+#include <iostream>
+#include "tabtenn0.h"
+
+TableTennisPlayer::TableTennisPlayer(const string & fn, const string & ln, bool ht)
+    :firstname(fn), lastname(ln), hasTable(ht)
+{
+}
+
+void TableTennisPlayer::Name() const
+{
+    std::cout << lastname << ", " << firstname;
+}
