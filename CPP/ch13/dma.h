3efc23951f8a97d18516e8dbd911b3d2547899bd
jerry <jerryhanjj@126.com>
2021-11-10 21:48:17 +0800

    1110 commit

--- CPP/ch13/dma.h
+++ CPP/ch13/dma.h
@@ -28,6 +28,7 @@ private:
         COL_LEN = 40
     };
     char color[COL_LEN];
+    
 public:
     lacksDMA(const char *c = "blank", const char * l = "null", int r = 0);
     lacksDMA(const char *c, const baseDMA &rs);
